d5a9474d5661b1b085354c2af3c435364e3bf0aa
ratneshrc <ratneshchavan349@gmail.com>
2026-07-24 15:42:02 +0530

    Create conditional.py

---
+++ Exp no.1/conditional.py
@@ -0,0 +1,10 @@
+Marks = int(input("Enter your marks:"))
+if(Marks>90):
+    print("A Grade")
+elif(Marks>75):
+    print("B Grade")
+elif(Marks>40):
+    print("C Grade")
+else:
+    print("Fail")
+    
