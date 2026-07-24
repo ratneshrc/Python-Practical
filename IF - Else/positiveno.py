a9894fcadda2a2e4f38e2668a0bec3c6c5d7dfc6
ratneshrc <ratneshchavan349@gmail.com>
2026-07-24 15:38:35 +0530

    Delete IF - Else directory

--- IF - Else/positiveno.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a number: "))
-
-if n > 0:
-    print("Positive Number")
-elif n < 0:
-    print("Negative Number")
-else:
-    print("Zero")
--- IF - Else/zero.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Enter a number: "))
-
-if n == 0:
-    print("The number is Zero")
-else:
-    print("The number is Non-Zero")
