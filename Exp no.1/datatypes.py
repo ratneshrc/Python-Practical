4d05afd0c10620e023fd2f30144a1a01423e3f20
ratneshrc <ratneshchavan349@gmail.com>
2026-07-24 15:42:35 +0530

    Create datatypes.py

---
+++ Exp no.1/datatypes.py
@@ -0,0 +1,31 @@
+# Integer (int)
+age = 20
+print(age)
+
+# Float
+height = 175
+print(height)
+
+# String (str)
+name = "Siddhesh"
+print(name)
+
+# Boolean (bool)
+student = True
+print(student)
+
+# List
+fruits = ["apple", "banana", "mango"]
+print(fruits)
+
+# Tuple
+colors = ("red", "green", "blue")
+print(colors)
+
+# Set
+rollnum = {11, 22, 33}
+print(rollnum)
+
+# NoneType
+result = None
+print(result)
