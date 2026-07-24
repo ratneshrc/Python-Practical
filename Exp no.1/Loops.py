5e7e4f3593fe0a95c771601b6e7b7acc73c75b8a
ratneshrc <ratneshchavan349@gmail.com>
2026-07-24 15:42:59 +0530

    Create Loops.py

---
+++ Exp no.1/Loops.py
@@ -0,0 +1,17 @@
+print("For Loop")
+for i in range(1, 5):
+    print(i)
+
+print("While Loop")
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+
+print("Do While Loop")
+i = 1
+while True:
+    print(i)
+    i += 1
+    if i >= 5:
+        break
