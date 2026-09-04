ef144c8b90b5bd79a82cce5b75c3a6419ef058ed
ratneshrc <ratneshchavan349@gmail.com>
2026-09-04 14:52:37 +0530

    Update Private_Protected.py

--- Exp no. 8/Private_Protected.py
+++ Exp no. 8/Private_Protected.py
@@ -1,15 +1,29 @@
 class A:
-    __private = 10
-    _protected = 20
+    def __init__(self):
+        self.__x = 10      # Private
+        self._y = 20       # Protected
+
+    def __private(self):
+        print("Private function")
+
+    def _protected(self):
+        print("Protected function")
 
     def show(self):
-        print(self.__private)
-        print(self._protected)
+        print(self.__x)
+        self.__private()
+
 
 class B(A):
-    pass
+    def display(self):
+        print(self._y)
+        self._protected()
+
 
 class C(B):
     pass
 
-C().show()
+
+obj = C()
+obj.show()
+obj.display()
